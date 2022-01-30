1a1f7546e27cce19885444ee2ba51334792603e2
Paulius Lazdauskas <paulius.lazdauskas1@gmail.com>
2022-01-31 01:47:13 +0200

    =m

---
+++ src/pages/ShoppingCart.tsx
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+import { Cart } from "../pages/CartPage/Cart/Cart";
+import { Layout } from "../pages/CartPage/Layout/Layout";
+import { SearchedItemDetails } from "../pages/CartPage/Layout/Search/SearchedItemDetails";
+import Modal from "../pages/CartPage/UI/Modal";
+
+import { Products } from "../pages/CartPage/UI/Products";
+import { RootState } from "../store";
+
+export const ShoppingCart = () => {
+  const showCart = useSelector((state: RootState) => state.ui.cartIsVisible);
+
+  const searchedItemDetailsOpened = useSelector(
+    (state: RootState) => state.ui.searchedItemDetailsOpened
+  );
+
+  return (
+    <div>
+      <Layout>
+        <Products />
+        {showCart && (
+          <Modal>
+            <Cart />
+          </Modal>
+        )}
+        {searchedItemDetailsOpened && (
+          <Modal>
+            <SearchedItemDetails />
+          </Modal>
+        )}
+      </Layout>
+    </div>
+  );
+};
